0644cd73b2cb5f003152176a6e7f83cb971f85a1
Vidith Agarwal <vidithagarwal315@gmail.com>
2024-11-25 01:53:27 -0500

    Initialized maven project, added spring dependencies, created fake payment service

---
+++ src/main/java/org/example/services/PaymentServiceImpl.java
@@ -0,0 +1,15 @@
+package org.example.services;
+
+public class PaymentServiceImpl implements PaymentService {
+  @Override
+  public void makePayment() {
+    // Payment code
+
+    System.out.println("Amount Debited...");
+
+
+    System.out.println("Amount Credited...");
+
+
+  }
+}
