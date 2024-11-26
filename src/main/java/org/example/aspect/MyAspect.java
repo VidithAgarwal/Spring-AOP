1da071e01f1167b94e34454932b94dca0e134ecd
Vidith Agarwal <vidithagarwal315@gmail.com>
2024-11-25 23:30:05 -0500

    Added before aspect to make payment method

--- src/main/java/org/example/aspect/MyAspect.java
+++ src/main/java/org/example/aspect/MyAspect.java
@@ -5,4 +5,9 @@ import org.aspectj.lang.annotation.Before;
 
 @Aspect
 public class MyAspect {
+
+  @Before("execution(* org.example.services.PaymentServiceImpl.makePayment())")
+  void logStartPayment() {
+    System.out.println("Payment Starting...");
+  }
 }
